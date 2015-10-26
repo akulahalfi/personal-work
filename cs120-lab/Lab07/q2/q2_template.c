8355359836e55a604652276762e624ee38b48d60
Alfie Lee <akulahalfi@aol.com>
2015-10-27 04:22:04 +0800

    Added Lab07

---
+++ cs120-lab/Lab07/q2/q2_template.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* Function declarations */
+void print_cubes(void);
+
+/*Don't change the main function!*/
+int main(void)
+{
+	/* Print all of the integers with the cube property */
+	print_cubes();
+	return 0;
+}
